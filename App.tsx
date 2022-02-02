5a5a58061dafb5ef6b004556a1f76fbe79c0440b
Bednaz98 <30347432+Bednaz98@users.noreply.github.com>
2022-02-02 15:51:36 -0500

    added component

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [first, setfirst] = useState(second);
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
