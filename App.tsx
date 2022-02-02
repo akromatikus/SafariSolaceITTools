2a13c73bafbb90d1b7ce9d0f147ec4b21842a51e
Bednaz98 <30347432+Bednaz98@users.noreply.github.com>
2022-02-02 16:14:28 -0500

    updated config

--- App.tsx
+++ App.tsx
@@ -1,13 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import BasicText from './safari-solaces-tyletools/basictext';
+import { ThemeContextInterface } from './safari-solaces-tyletools/themecontext';
+
+
+
 
 export default function App() {
-  const [first, setfirst] = useState(second);
+  const [pageIndx, setPageIndex] = useState(1);
+  // const [user, setfirst] = useState(false);
+  // const [employeeList, setfirst] = useState(false);
+  // const [employeeIndex, setfirst] = useState(false);
+  // const [theme, setfirst] = useState(false);
+  // const [sync, setfirst] = useState(true);
+  // const [theme, setTheme] = useState(3);
+
+  function switchDisplay(){
+    switch(pageIndx){
+      case 0: {return (<BasicText text={"Login in page"}/>)  }
+      case 1: {return (<BasicText text={"Main page"}/>)}
+      case 2: { return (<BasicText text={"User setting"}/>)}
+    }
+  }
+  //const themeContextObject:ThemeContextInterface = {theme:theme,setTheme:setTheme}
+  const contxt = {
+
+  }
+  
   
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      {switchDisplay()}
+     
     </View>
   );
 }
@@ -15,7 +41,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#999',
     alignItems: 'center',
     justifyContent: 'center',
   },
